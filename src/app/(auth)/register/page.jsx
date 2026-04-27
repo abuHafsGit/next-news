731092d66dd4db23e17e327cffe899921d6ad11d
abuHafsGit <muhammadabuhafz@gmail.com>
2026-04-27 11:47:02 +0600

    feat: add react-hook-form for improved registration handling and update auth client URI

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,99 +1,124 @@
 "use client";
+import { authClient } from "@/lib/auth-client";
+import Link from "next/link";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
-import { Check, Eye, EyeSlash } from "@gravity-ui/icons";
-import { Button, Description, FieldError, Form, Input, InputGroup, Label, TextField } from "@heroui/react";
-import { useState } from "react";
+const RegisterPage = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm();
 
-const ResgisterPage = () => {
-    const [isVisible, setIsVisible] = useState(false);
-    const onSubmit = (e) => {
-        e.preventDefault();
-        const name = e.target.name.value;
-        const photo_url = e.target.photo.value;
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        const userData = { name, photo_url, email, password }
-        console.log(userData)
-    }
+    const [isShowPassword, setIsShowPassword] = useState(false);
 
+    const handleRegisterFunc = async (data) => {
+        console.log(data, "data");
+        const { email, name, photo, password } = data;
+        console.log(name, photo);
+
+        const { data: res, error } = await authClient.signUp.email({
+            name: name, // required
+            email: email, // required
+            password: password, // required
+            image: photo,
+            callbackURL: "/",
+        });
+
+        console.log(res, error);
+        if (error) {
+            alert(error.message);
+        }
+
+        if (res) {
+            alert("Signup successful");
+        }
+    };
 
     return (
-        <div className=" container mx-auto justify-center items-center">
-            <div className=" w-80 mx-auto h-[80vh] flex justify-center items-center">
-                <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
-                    <TextField
-                        isRequired
-                        name="name"
-                        type="text"
-                    >
-                        <Label>Name</Label>
-                        <Input placeholder="Write your name" />
-                        <FieldError />
-                    </TextField>
-                    <TextField
-                        isRequired
-                        name="photo"
-                        type="text"
-                    >
-                        <Label>Photo Url</Label>
-                        <Input placeholder="Your profile pic url" />
-                        <FieldError />
-                    </TextField>
-                    <TextField
-                        isRequired
-                        name="email"
-                        type="email"
-                        validate={(value) => {
-                            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
-                                return "Please enter a valid email address";
-                            }
+        <div className="container mx-auto min-h-[80vh] flex justify-center items-center bg-slate-100">
+            <div className="p-4 rounded-xl bg-white">
+                <h2 className="font-bold text-3xl text-center mb-6">
+                    Register your account
+                </h2>
 
-                            return null;
-                        }}
-                    >
-                        <Label>Email</Label>
-                        <Input placeholder="john@example.com" />
-                        <FieldError />
-                    </TextField>
+                <form className="space-y-4" onSubmit={handleSubmit(handleRegisterFunc)}>
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Name</legend>
+                        <input
+                            type="text"
+                            className="input"
+                            placeholder="Type here name"
+                            {...register("name", {
+                                required: "Name field is required",
+                            })}
+                        />
+                        {errors.name && (
+                            <p className="text-red-500">{errors.name.message}</p>
+                        )}
+                    </fieldset>
 
-                    <TextField className="w-full " name="password">
-                        <Label>Password</Label>
-                        <InputGroup>
-                            <InputGroup.Input
-                                className="w-full "
-                                type={isVisible ? "text" : "password"}
-                                placeholder="write password"
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Photo URL</legend>
+                        <input
+                            type="text"
+                            className="input"
+                            placeholder="Type here photo url"
+                            {...register("photo", {
+                                required: "Photo URL field is required",
+                            })}
+                        />
+                        {errors.photo && (
+                            <p className="text-red-500">{errors.photo.message}</p>
+                        )}
+                    </fieldset>
 
-                            />
-                            <InputGroup.Suffix className="pr-0">
-                                <Button
-                                    isIconOnly
-                                    aria-label={isVisible ? "Hide password" : "Show password"}
-                                    size="sm"
-                                    variant="ghost"
-                                    onPress={() => setIsVisible(!isVisible)}
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Email</legend>
+                        <input
+                            type="email"
+                            className="input"
+                            placeholder="Type here email"
+                            {...register("email", {
+                                required: "Email field is required",
+                            })}
+                        />
+                        {errors.email && (
+                            <p className="text-red-500">{errors.email.message}</p>
+                        )}
+                    </fieldset>
 
-                                >
-                                    {isVisible ? <Eye className="size-4" /> : <EyeSlash className="size-4" />}
-                                </Button>
-                            </InputGroup.Suffix>
-                        </InputGroup>
-                    </TextField>
+                    <fieldset className="fieldset relative">
+                        <legend className="fieldset-legend">Password</legend>
+                        <input
+                            type={isShowPassword ? "text" : "password"}
+                            className="input"
+                            placeholder="Type here password"
+                            {...register("password", {
+                                required: "Password field is required",
+                            })}
+                        />
+                        <span
+                            className="absolute right-8 top-4 cursor-pointer"
+                            onClick={() => setIsShowPassword(!isShowPassword)}
+                        >
+                            {isShowPassword ? <FaEye /> : <FaEyeSlash />}
+                        </span>
+                        {errors.password && (
+                            <p className="text-red-500">{errors.password.message}</p>
+                        )}
+                    </fieldset>
 
-                    <div className="flex gap-2">
-                        <Button type="submit">
-                            <Check />
-                            Submit
-                        </Button>
-                        <Button type="reset" variant="secondary">
-                            Reset
-                        </Button>
-                    </div>
-
-                </Form>
+                    <button className="btn w-full bg-slate-800 text-white">
+                        Register
+                    </button>
+                </form>
             </div>
         </div>
     );
-}
+};
 
-export default ResgisterPage
+export default RegisterPage;
